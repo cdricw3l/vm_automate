bd216aee894ddc4191a87a80cd84a94f8934bd17
cdric.B <cdric.b@Mac.lan>
2026-04-12 22:41:57 +0200

    id

--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -1,78 +1,38 @@
 # include "dispatcher.h"
 # include <sqlite3.h>
 
-char *get_pub_key(void)
+
+int get_pub_key(t_id *id)
 {
 	int fd;
 	char *pub_key_path;
 	char *line;
 	char **split;
-	char *key;
 
 	pub_key_path = getenv("SSH_USER_AUTH");
 	if(!pub_key_path)	
 	{
 		printf("path error\n");
-		return (NULL);
+		return (ERR);
 	}
 	fd = open(pub_key_path, O_RDONLY);
 	if(fd < 0)
 	{
 		perror("read error");
-		return (NULL);
-	}
-	else
-		printf("fileDescriptor %d\n", fd);
-	
+		return (ERR);
+	}	
 	line = get_next_line(fd);
+	close(fd);
 	if(!line)
-	{
-		close(fd);
-		return (NULL);
-	}
+		return (ERR);
 	split = ft_split(line, 32);
 	free(line);
 	if(!split)
-	{
-		close(fd);
-		return (NULL);
-	}
-	key = ft_strdup(split[2]);
+		return (ERR);
+	id->pub_key = ft_strdup(split[2]);
 	clean_split(split);
-	return (key);
+	return (OK);
 }
-
-
-// int main(void)
-// {
-
-// 	char *pub_key;
-// 	int db;
-// 	sqlite3 *ppdb;
-
-// 	pub_key = get_pub_key();
-// 	if(!pub_key)
-// 		return (1);
-// 	printf("key: %s\n", pub_key);
-// 	db = sqlite3_open(DB, &ppdb);
-// 	printf("voici db %d\n", db);
-// 	free(pub_key);
-// 	return(0);
-
-// }
-
-// int main(void)
-// {
-
-// 	int db;
-// 	sqlite3 *ppdb;
-
-// 	db = sqlite3_open(DB, &ppdb);
-// 	assert(db == SQLITE_OK);
-// 	return(0);
-
-// }
-
 
 
 int check_and_dispatch(char *arg)
@@ -101,31 +61,43 @@ int check_and_dispatch(char *arg)
 			code = RESTART_VM;
 		else if(!strcmp(args[i], "vm_off"))
 			code = SHUTDOWN_VM;
+		else if(!strcmp(args[i], "help"))
+			code = HELP;
 		i++;
 	}
 	clean_split(args);	
 	return(code);
 }
 
+void	ft_print_help()
+{
+	printf("Usage:\n");
+	printf("  -vmc   Create a virtual machine\n");
+	printf("  -vmr   Reboot a virtual machine\n");
+	printf("  -vms   Create a snapshot of a virtual machine\n");
+	printf("  -vmsd  Shutdown a virtual machine\n");
+	printf("  -help  Display this help message\n");
+}
 int main(void)
 {
 	char *arg;
-	char *pub_key;
+	t_id id;
 	int dispatch_code;
 
 	arg = getenv("SSH_ORIGINAL_COMMAND");
 	if(!arg)
 	{
 		printf("You need to give an argument\n");
+		ft_print_help();
 		return (1);
 	}
-	
-	pub_key = get_pub_key();
-	if(!pub_key)
+	;
+	if(get_pub_key(&id) == ERR)
+	{
+		printf("get public key error\n");
 		return (1);
-	printf("key: %s\n", pub_key);
+	}
 	dispatch_code = check_and_dispatch(arg);
-	printf("voici %d\n", dispatch_code);
 	if(dispatch_code == DISPATCH_ERR)
 		printf("Argument processing error\n");
 	else if( dispatch_code == CREATE_VM)
@@ -136,5 +108,9 @@ int main(void)
 		printf("You are asking for a  snapshot vm\n");
 	else if( dispatch_code == SHUTDOWN_VM)
 		printf("You are asking for a  shutdown  of vm\n");
+	else if( dispatch_code == HELP)
+		ft_print_help();
 	return (0);
 }
+
+
--- srcs/dispatcher.h
+++ srcs/dispatcher.h
@@ -15,6 +15,17 @@
 #define RESTART_VM      1
 #define SNAPSHOT_VM     2
 #define SHUTDOWN_VM     3
-#define DISPATCH_ERR    4
+#define HELP            4
+#define DISPATCH_ERR    5
+
+#define ERR             0
+#define OK              1
+
+typedef struct s_id
+{
+	char *user;
+	char *pub_key;
+
+} t_id;
 
 #endif
